d1f105a03b925fee0feaa12dedb1bffd74ecfa47
Christian Stewart <kidovate@gmail.com>
2015-01-05 15:06:19 -0800

    Initial commit

---
+++ src/Commands/Autonomous.cpp
@@ -0,0 +1,10 @@
+#include "Autonomous.h"
+
+#include <iostream>
+
+Autonomous::Autonomous() : CommandGroup("Autonomous") {
+	//Add your autonomous commands, for example:
+	//AddSequential(new PrepareToPickup());
+    //AddParallel(new SetWristSetpoint(-45));
+    //AddSequential(new CloseClaw());
+}
---
+++ src/Robot.cpp
@@ -0,0 +1,50 @@
+
+#include "Robot.h"
+#include "Commands/Autonomous.h"
+
+//Initialize each of the components of the robot:
+//DriveTrain* Robot::drivetrain = NULL;
+
+OI* Robot::oi = NULL;
+
+void Robot::RobotInit() {
+	std::cout << "FRC Team 1148 robot code startup" << std::endl;
+	//Init all the components
+	//drivetrain = new DriveTrain();
+
+	oi = new OI();
+
+	autonomousCommand = new Autonomous();
+	lw = LiveWindow::GetInstance();
+
+    // Show what command your subsystem is running on the SmartDashboard
+    //SmartDashboard::PutData(drivetrain);
+}
+
+void Robot::AutonomousInit() {
+	autonomousCommand->Start();
+	std::cout << "Starting Auto" << std::endl;
+}
+
+void Robot::AutonomousPeriodic() {
+	Scheduler::GetInstance()->Run();
+}
+
+void Robot::TeleopInit() {
+	// This makes sure that the autonomous stops running when
+	// teleop starts running. If you want the autonomous to
+	// continue until interrupted by another command, remove
+	// this line or comment it out.
+	autonomousCommand->Cancel();
+	std::cout << "Starting Teleop" << std::endl;
+}
+
+void Robot::TeleopPeriodic() {
+	Scheduler::GetInstance()->Run();
+}
+
+void Robot::TestPeriodic() {
+	lw->Run();
+}
+
+START_ROBOT_CLASS(Robot);
